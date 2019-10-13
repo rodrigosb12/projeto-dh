cfd74601c9374c3bcc2b0bd09d9553b724f08ff5
EduFStack <55258174+EduFStack@users.noreply.github.com>
2019-10-13 12:36:06 -0300

    Primeira versao da tela de cadastro

---
+++ cadastro.php
@@ -0,0 +1,185 @@
+<!doctype html>
+<html lang="pt-br">
+
+<head>
+    <title>Registrar - Wimart</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="estilo/estilo-signup.css">
+    <link rel="stylesheet" href="estilo/estilo.css">
+</head>
+
+<body>
+    <header>
+        <div class="container">
+            <div class="row">
+                <a class="navbar-brand" href="index.html">
+                    <img src="img/logos/Wimart-logo-right-icon.png" alt="logotipo">
+                </a>
+                <div class="col d-flex align-items-center">
+                    <ul class="nav">
+                        <li class="nav-item">
+                            <a class="nav-link text-uppercase" href="#">Shop</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-uppercase" href="#">Studio</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-uppercase" href="#">Comunidades</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col d-flex align-items-center justify-content-end">
+                    <form class="form-inline">
+                        <input class="form-control mr-sm-0 text-uppercase" type="busca" placeholder="Busca"
+                            aria-label="Busca">
+                        <button class="btn btn-outline-seconday my-2 my-sm-0 text-uppercase"
+                            type="submit">Buscar</button>
+                    </form>
+                    <ul class="nav">
+                        <li class="nav-item">
+                            <a class="nav-link text-uppercase" href="#">Login</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-uppercase" href="#">Registrar</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </header>
+    <section class="container stilo">
+
+        <form>
+            <div>
+                <div class="form-group col-md-12">
+                <label for="nome">Nome</label>
+                    <input type="text" class="form-control" placeholder="Nome">
+                </div>
+                <div class="form-group col-md-12">
+                <label for="sobrenome">Sobrenome</label>
+                    <input type="text" class="form-control" placeholder="Sobrenome">
+                </div>
+            </div>
+            <div>
+                <div class="form-group col-md-12">
+                    <label for="inputEmail4">Email</label>
+                    <input type="email" class="form-control" id="inputEmail4" placeholder="Email">
+                </div>
+                <div class="form-group col-md-12">
+                    <label for="inputPassword4">Senha</label>
+                    <input type="password" class="form-control" id="inputPassword4" placeholder="Senha">
+                </div>
+                <div class="form-group col-md-12">
+                    <label for="inputPassword4">Confirme sua senha</label>
+                    <input type="password" class="form-control" id="inputPassword4" placeholder="Confirme sua Senha">
+                </div>
+            </div>
+            <div class="form-group col-md-12">
+                <label for="inputAddress">Endereço</label>
+                <input type="text" class="form-control" id="inputAddress" placeholder="Ex: Avenida Paulista, nº 100">
+            </div>
+            <div>
+                <div class="form-group col-md-12">
+                    <label for="inputCity">Cidade</label>
+                    <input type="text" class="form-control" id="inputCity">
+                </div>
+                <div class="form-group col-md-4">
+                    <label for="inputEstado">Estado</label>
+                    <select id="inputEstado" class="form-control">
+                        <option selected>Escolher...</option>
+                        <option>...</option>
+                    </select>
+                </div>
+                <div class="form-group col-md-6">
+                    <label for="inputCEP">CEP</label>
+                    <input type="text" class="form-control" id="inputCEP">
+                </div>
+            </div>
+            <!--<div class="form-group col-md-6">
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" id="gridCheck">
+                    <label class="form-check-label" for="gridCheck">
+                        Clique em mim  
+                    </label>
+                </div>
+            </div>-->
+            <div class="form-group ">
+                <button type="submit" class="btn btn-outline-primary">Avançar</button>
+            </div>
+        </form>
+
+    </section>
+    <footer>
+        <div class="container py-4">
+            <div class="row">
+                <div class="col logotipo">
+                    <a href="index.html">
+                        <img src="img/logos/Wimart-logo-white-top-icon.png" alt="logotipo">
+                    </a>
+                </div>
+                <div class="col sobre">
+                    <h3 class="footer-subtitulo">Sobre</h3>
+                    <ul class="footer-listas">
+                        <li>
+                            <a href="#">Sobre o Wimart</a>
+                        </li>
+                        <li>
+                            <a href="#">Sobre o Wimart Studio</a>
+                        </li>
+                        <li>
+                            <a href="#">Trabalhe conosco</a>
+                        </li>
+                        <li>
+                            <a href="#">Contato</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col">
+                    <h3 class="footer-subtitulo">Suporte</h3>
+                    <ul class="footer-listas">
+                        <li>
+                            <a href="#">Regras gerais</a>
+                        </li>
+                        <li>
+                            <a href="#">FAQ</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col">
+                    <h3 class="footer-subtitulo">Siga o Wimart</h3>
+                    <a href="#">
+                        <img src="img/icons/iconfinder_2018_social_media_popular_app_logo_facebook_3225194.svg"
+                            alt="icon-facebook" class="social-media-icons m-1">
+                    </a>
+                    <a href="#">
+                        <img src="img/icons/iconfinder_2018_social_media_popular_app_logo_instagram_3225191.svg"
+                            alt="icon-facebook" class="social-media-icons m-1">
+                    </a>
+                    <a href="#">
+                        <img src="img/icons/iconfinder_2018_social_media_popular_app_logo_twitter_3225183.svg"
+                            alt="icon-facebook" class="social-media-icons m-1">
+                    </a>
+                    <a href="#">
+                        <img src="img/icons/iconfinder_2018_social_media_popular_app_logo_linkedin_3225190.svg"
+                            alt="icon-facebook" class="social-media-icons m-1">
+                    </a>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
