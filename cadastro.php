83fa24300d5683e1bc7a149fbeb5414b7278bffe
EduFStack <55258174+EduFStack@users.noreply.github.com>
2019-10-13 21:21:07 -0300

    primeira versão da modal de login

--- cadastro.php
+++ cadastro.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="estilo/estilo-signup.css">
+    <link rel="stylesheet" href="estilo/estilo-cadastro.css">
     <link rel="stylesheet" href="estilo/estilo.css">
 </head>
 
---
+++ login.php
@@ -0,0 +1,97 @@
+<!doctype html>
+<html lang="pt-br">
+
+<head>
+    <title>Registrar - Wimart</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="estilo/estilo-login.css">
+
+</head>
+
+<body>
+
+    <section class="container">
+        <!-- Botão para acionar modal -->
+        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modalExemplo">
+            Abrir modal de demonstração
+        </button>
+
+        <!-- Modal -->
+        <div class="modal fade" id="modalExemplo" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+            aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Acesse o Wimart</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body container">
+                        <button type="button" class="btn btn-primary col-md-12 botao">
+                            <img class="estilo-logo"
+                                src="img/icons/iconfinder_2018_social_media_popular_app_logo_facebook_3225194.svg"
+                                alt="logo Facebook">
+                            <p class="estilo-paragrafo">ENTRAR COM FACEBOOK</p>
+
+                        </button>
+                        <button type="button" class="btn btn-light col-md-12 botao">
+                            <img class="estilo-logo"
+                                src="img/icons/iconfinder_2018_social_media_popular_app_logo_facebook_3225194.svg"
+                                alt="logo Facebook">
+                            <p class="estilo-paragrafo">ENTRAR COM GOOGLE</p>
+                        </button>
+                        <div>
+                            <p class="estilo-paragrafo">OU</p>
+                        </div>
+                        
+                        <form>
+                            <div class="form-group">
+                                <label for="exampleInputEmail1">Email</label>
+                                <input type="email" class="form-control" id="exampleInputEmail1"
+                                    aria-describedby="emailHelp" placeholder="Seu email">
+                            </div>
+                            <div class="form-group">
+                                <label for="exampleInputPassword1">Senha</label>
+                                <input type="password" class="form-control" id="exampleInputPassword1"
+                                    placeholder="Senha">
+                            </div>
+                            <div class="form-group form-check">
+                                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                            </div>
+                            <button type="submit" class="btn btn-info col-md-12">LOG IN</button>
+                        </form>
+                        <div>
+                            <p class="estilo-paragrafo2">Esqueçeu a senha?</p>
+                        </div>
+                        
+                    </div>
+                    <div class="modal-content">
+                        <h2 class="estilo-paragrafo3">Ainda não é membro? Clique aqui!</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+
+
+
+    </section>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
