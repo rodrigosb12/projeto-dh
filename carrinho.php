389133d940239f4bf375b5fb9ac53a8084dbf640
EduFStack <55258174+EduFStack@users.noreply.github.com>
2019-10-30 19:22:15 -0300

    e continuando...

--- carrinho.php
+++ carrinho.php
@@ -1,26 +1,57 @@
 <?php require_once('header.php'); ?>
 
-<section class="container">
-
-<div class="row ">
-
-    <div class="row col-9 my-2 background-carrinho align-items-center justify-content-between pl-0">
-        <img src="img/produtos/audio-7.jpg" alt="produto" class="col-lg-1 img-carrinho">
-        <div class="row col-lg-11 ml-1 justify-content-between align-items-center px-0">
-            <h1 class="col-lg-auto font-carrinho px-0">Nome do produto</h1>
-            <h2 class="col-auto font-carrinho">Preço</h2>
+<section class=" container">
+    <div class="row justify-content-between">
+        <div class="row col-9 my-2 background-carrinho justify-content-between align-items-start pl-0 mr-1">
+            <img src="img/produtos/audio-7.jpg" alt="produto" class="col-1 teste px-0 img-carrinho">
+            <div class="row col-lg-11 my-2 justify-content-between teste">
+                <h1 class="col-lg-auto font-carrinho px-0">Nome do produto</h1>
+                <h2 class="col-auto font-carrinho">Preço</h2>
+            </div>
+            <!-- <div class="row align-items-start">
+            <h4 class="font-carrinho">Selecione a quantidade:</h4>
+            </div> -->
+        </div>
+        
+        <div class="row pr-0 my-2 col-lg-3 teste1">
+            <div class="row py-1 mt-2 col-12">
+                <h2 class="font-carrinho">Detalhes da Compra</h2>
+            </div>
+            <div class="row col-12 pr-0 justify-content-between">
+                <h3 class="col-8 px-0 font-carrinho">Quantidade</h3>
+                <h3 class="col-auto font-carrinho">1</h3>
+            </div>
+            <div class="row col-12 pr-0 justify-content-between">
+                <h3 class="col-8 px-0 font-carrinho">Subtotal</h3>
+                <h3 class="col-auto font-carrinho">1</h3>
+            </div>
+            <div class="row col-12 pr-0 justify-content-between">
+                <h3 class="col-8 px-0 font-carrinho">Frete</h3>
+                <h3 class="col-auto font-carrinho">1</h3>
+            </div>
+            <div class="row col-12 pr-0 justify-content-between">
+                <h3 class="col-8 px-0 font-carrinho">Total</h3>
+                <h3 class="col-auto font-carrinho">1</h3>
+            </div>
+            <button class="btn btn-primary">Comprar</button>
         </div>
     </div>
-    
-    <div class="row col-3 my-2 background-carrinho align-items-center justify-content-between pl-0">
-        <img src="img/produtos/audio-7.jpg" alt="produto" class="col-lg-1 img-carrinho">
-        <div class="row col-lg-11 ml-1 justify-content-between align-items-center px-0">
-            <h1 class="col-lg-auto font-carrinho px-0">Nome do produto</h1>
-            <h2 class="col-auto font-carrinho">Preço</h2>
-        </div>
+
+    <!-- <div class="row background-carrinho">
+    <div class="row col-9">
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias similique, dolorum magnam deserunt officiis nisi iure! Vero culpa illum nihil natus quod sunt ducimus aut alias accusamus in? Quia, quisquam.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa facilis, voluptas, in labore quod ad atque numquam perspiciatis iure aut velit dolorum nemo dolorem eum molestiae ab, eligendi cum minus!</p>  
+    </div>
+    <div class="row col-3">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet corporis blanditiis rerum optio in, odio reprehenderit aliquam sint quis nesciunt architecto quisquam accusamus fuga dolor nobis alias, quaerat aliquid reiciendis!</p>
     </div>
 
-</div>
+
+</div> -->
+
+
+
+
 
 
 </section>
