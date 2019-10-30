7edb8216a32ef7a8641e248273dcfcd43f544d03
EduFStack <55258174+EduFStack@users.noreply.github.com>
2019-10-30 09:41:17 -0300

    trabalhando no carrinho

--- carrinho.php
+++ carrinho.php
@@ -1,57 +1,27 @@
 <?php require_once('header.php'); ?>
 
-<section class="container-fluid">
+<section class="container">
 
-    <div class="row no-gutters col-12">
-        <div class="row no-gutters my-2 align-items-center justify-content-between background-carrinho col-6">
-            <img src="img/produtos/audio-7.jpg" alt="produto" class="col-auto img-carrinho">
-            <div class="row no-gutters col-auto justify-content-between">
-                <h1 class="col-auto font-carrinho">Nome do produto</h1>
-                <h2 class="col-1 font-carrinho">Preço</h2>
-            </div>
+<div class="row ">
+
+    <div class="row col-9 my-2 background-carrinho align-items-center justify-content-between pl-0">
+        <img src="img/produtos/audio-7.jpg" alt="produto" class="col-lg-1 img-carrinho">
+        <div class="row col-lg-11 ml-1 justify-content-between align-items-center px-0">
+            <h1 class="col-lg-auto font-carrinho px-0">Nome do produto</h1>
+            <h2 class="col-auto font-carrinho">Preço</h2>
         </div>
-        <div class="row no-gutters my-2 align-items-center justify-content-between background-carrinho col-6">
-            <img src="img/produtos/audio-7.jpg" alt="produto" class="col-auto img-carrinho">
-            <div class="row no-gutters col-auto justify-content-between">
-                <h1 class="col-auto font-carrinho">Nome do produto</h1>
-                <h2 class="col-1 font-carrinho">Preço</h2>
-            </div>
+    </div>
+    
+    <div class="row col-3 my-2 background-carrinho align-items-center justify-content-between pl-0">
+        <img src="img/produtos/audio-7.jpg" alt="produto" class="col-lg-1 img-carrinho">
+        <div class="row col-lg-11 ml-1 justify-content-between align-items-center px-0">
+            <h1 class="col-lg-auto font-carrinho px-0">Nome do produto</h1>
+            <h2 class="col-auto font-carrinho">Preço</h2>
         </div>
     </div>
 
+</div>
 
-    <!-- <div class="row">
-        <div class="row ml-2 my-3 div-color">
-                <img src="" alt="produto" class="col my-2">
-                <h2 class="col">Produto selecionado</h2>
-                <h2 class=" col text-right">Preço</h2>
-            </div>
-        <div class="col">
-            <form method="POST">
-                <div class="form-group my-2 text-left">
-                    <label for="modelos-disponiveis" class="mb-0"> Modelos disponíveis</label>
-                    <select class="form-control" id="modelos-disponiveis">
-                        <option>Modelo1</option>
-                        <option>Modelo2</option>
-                        <option>Modelo3</option>
-                        <option>Modelo4</option>
-                        <option>Modelo5</option>
-                    </select>
-                </div>
-                <div class="form-group row col-auto my-3">
-                    <label for="selecionar-quantidade" class="my-2">Quantidade</label>
-                    <select class="form-control col-2 ml-2" id="selecionar-quantidade">
-                        <option>1</option>
-                        <option>2</option>
-                        <option>3</option>
-                        <option>4</option>
-                        <option>5</option>
-                    </select>
-                </div>
-            </form>
-
-        </div>
-    </div> -->
 
 </section>
 
