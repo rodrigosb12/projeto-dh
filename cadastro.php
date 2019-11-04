060b49d52ae344c0e35b11aeea01dab610d62739
EduFStack <55258174+EduFStack@users.noreply.github.com>
2019-11-03 21:11:08 -0300

    versão final carrinho/cadastro

--- cadastro.php
+++ cadastro.php
@@ -1,65 +1,80 @@
 <?php require_once('header.php'); ?>
-    <section class="container stilo">
-        <h1>CADASTRO</h1>
-        <form>
-            <div>
-                <div class="form-group col-md-12">
-                    <label for="nome">Nome</label>
-                    <input type="text" class="form-control" placeholder="Nome">
-                </div>
-                <div class="form-group col-md-12">
-                    <label for="sobrenome">Sobrenome</label>
-                    <input type="text" class="form-control" placeholder="Sobrenome">
-                </div>
-            </div>
-            <div>
-                <div class="form-group col-md-12">
-                    <label for="inputEmail4">Email</label>
-                    <input type="email" class="form-control" id="inputEmail4" placeholder="Email">
-                </div>
-                <div class="form-group col-md-12">
-                    <label for="inputPassword4">Senha</label>
-                    <input type="password" class="form-control" id="inputPassword4" placeholder="Senha">
-                </div>
-                <div class="form-group col-md-12">
-                    <label for="inputPassword4">Confirme sua senha</label>
-                    <input type="password" class="form-control" id="inputPassword4" placeholder="Confirme sua Senha">
-                </div>
-            </div>
-            <div class="form-group col-md-12">
-                <label for="inputAddress">Endereço</label>
-                <input type="text" class="form-control" id="inputAddress" placeholder="Ex: Avenida Paulista, nº 100">
-            </div>
-            <div>
-                <div class="form-group col-md-12">
-                    <label for="inputCity">Cidade</label>
-                    <input type="text" class="form-control" id="inputCity">
-                </div>
-                <div class="form-group col-md-4">
-                    <label for="inputEstado">Estado</label>
-                    <select id="inputEstado" class="form-control">
-                        <option selected>Escolher...</option>
-                        <option>...</option>
-                    </select>
-                </div>
-                <div class="form-group col-md-6">
-                    <label for="inputCEP">CEP</label>
-                    <input type="text" class="form-control" id="inputCEP">
-                </div>
-            </div>
-            <!--<div class="form-group col-md-6">
-                <div class="form-check">
-                    <input class="form-check-input" type="checkbox" id="gridCheck">
-                    <label class="form-check-label" for="gridCheck">
-                        Clique em mim  
-                    </label>
-                </div>
-            </div>-->
-            <div class="form-group ">
-                <button type="submit" class="btn btn-outline-primary">Avançar</button>
-            </div>
-        </form>
 
+<body class="background-cadastro">
+    <section class="container pt-3">
+        <div class="row col-12  justify-content-center ">
+            <div
+                class="row ml-2 col-12 col-lg-12 justify-content-center align-self-center background-row border rounded">
+                <h2 class="font-cadastro">Cadastro</h2>
+            </div>
+            <div class="row col-12 background-cadastro no-gutters px-0">
+                <br>
+            </div>
+            <form class="row col-12 ml-2 background-row border rounded">
+                <div class="col-12 col-lg-6">
+                    <div class="row col-12 pt-3 ">
+                        <div class="form-group row col-12">
+                            <label for="nome">Nome</label>
+                            <input type="text" class="form-control" placeholder="Nome">
+                        </div>
+                        <div class="form-group row col-12">
+                            <label for="sobrenome">Sobrenome</label>
+                            <input type="text" class="form-control" placeholder="Sobrenome">
+                        </div>
+                    </div>
+                    <div class="row col-12">
+                        <div class="form-group row col-12">
+                            <label for="inputEmail4">Email</label>
+                            <input type="email" class="form-control" id="inputEmail4" placeholder="Email">
+                        </div>
+                        <div class="form-group row col-12">
+                            <label for="inputPassword4">Senha</label>
+                            <input type="password" class="form-control" id="inputPassword4" placeholder="Senha">
+                        </div>
+                        <div class="form-group row col-12">
+                            <label for="inputPassword4">Confirme sua senha</label>
+                            <input type="password" class="form-control" id="inputPassword4"
+                                placeholder="Confirme sua Senha">
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-sm-12">
+                    <div class="row col-12 pt-3">
+                        <div class="form-group row col-12">
+                            <label for="inputAddress">Endereço</label>
+                            <input type="text" class="form-control" id="inputAddress"
+                                placeholder="Ex: Avenida Paulista, nº 100">
+                        </div>
+                        <div class="form-group row col-12">
+                            <label for="inputAddress" class="col-12 px-0">Bairro</label>
+                            <input type="text" class="form-control col-6" id="inputAddress" placeholder="">
+                        </div>
+                    </div>
+                    <div class="row col-12">
+                        <div class="form-group row col-12">
+                            <label for="inputCity" class="col-12 px-0">Cidade</label>
+                            <input type="text" class="form-control col-6" id="inputCity">
+                        </div>
+                        <div class="form-group row col-12">
+                            <label for="inputEstado" class="col-12 px-0">Estado</label>
+                            <select id="inputEstado" class="form-control col-6">
+                                <option selected>Escolher...</option>
+                                <option>...</option>
+                            </select>
+                        </div>
+                        <div class="form-group row col-12">
+                            <label for="inputCEP" class="col-12 px-0">CEP</label>
+                            <input type="text" class="form-control col-3" id="inputCEP">
+                        </div>
+                    </div>
+                </div>
+            </form>
+            <div class="row ml-2 col-12 pr-0 justify-content-end">
+                <div class="form-group row col-12 justify-content-end">
+                    <button type="submit" class="btn btn-primary">Finalizar</button>
+                </div>
+            </div>
+        </div>
     </section>
-    
+</body>
 <?php require_once('footer.php'); ?>
--- carrinho.php
+++ carrinho.php
@@ -2,9 +2,9 @@
 
 <body class="background-carrinho">
     <section class="container-fluid">
-        <div class="row justify-content-between pl-5">
+        <div class="row justify-content-between justify-content-center pl-5">
             <div
-                class="row col-lg-8 col-md-8 col-sm-12 my-3 align-self-start justify-content-between align-items-start">
+                class="row col-lg-8 col-md-12 col-sm-12 my-3 align-self-start justify-content-between align-items-start">
                 <div class="row col-12 background-row pl-0 border rounded">
                     <img src="img/produtos/audio-7.jpg" alt="produto" class="pl-0 col-lg-1 col-md-12">
                     <div class="row col-11 my-2 justify-content-between align-items-center">
@@ -41,7 +41,7 @@
                     </form>
                 </div>
             </div>
-            <div class="row col-lg-4 col-md-8">
+            <div class="row col-lg-4 col-md-12">
                 <div class="row pb-2 my-3 col-12 background-row border rounded">
                     <div class="row py-1 mt-2 col-auto">
                         <h2 class="font-produto">Detalhes da Compra</h2>
@@ -62,18 +62,20 @@
                         <h3 class="col-auto px-0 font-menu">Total</h3>
                         <h3 class="col-auto font-menu">R$ 123,98 </h3>
                     </div>
-                    <button class="btn btn-primary">Finalizar Compra</button>
+                    <div class="row col-12 justify-content-end ">
+                        <button class="btn btn-primary col-auto">Finalizar Compra</button>
+                    </div>
                 </div>
             </div>
 
         </div>
     </section>
     <section class="container-fluid">
-        <div class="row pl-5 col-lg-8 col-md-8 col-sm-12 pr-4 justify-content-between">
+        <div class="row pl-5 col-lg-8 col-md-12 col-sm-12 pr-4 justify-content-between">
             <div class="row pt-2 pr-0 col-lg-6 background-row align-items-center border rounded">
-                <h2 class="col-12 font-carrinho font-preco">Opções de Pagamento:</h2>
+                <h2 class="col-12 font-carrinho font-preco">Opções de Pagamento</h2>
                 <div class="row align-items-center pl-3">
-                    <div class="form-check col-lg-12 pt-2">
+                    <div class="form-check col-lg-12 pt-1">
                         <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1"
                             value="opcao1" checked>
                         <label class="form-check-label" for="exampleRadios1">
@@ -143,106 +145,68 @@
                         </div>
                     </div>
                 </form>
-
                 <div class="row align-items-center pl-3 pb-2">
                     <div class="form-check col-lg-12">
                         <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2"
                             value="opcao2">
                         <label class="form-check-label" for="exampleRadios2">
-                            PayPal
+                            <img class="col-3 px-0" src="img/icons/PayPal_logo.svg" alt="">
                         </label>
+
                     </div>
                 </div>
             </div>
-            <div class="row pt-2 pr-0 col-lg-6 background-row align-items-center border rounded">
-                <h2 class="col-12 font-carrinho font-preco">Opções de Pagamento:</h2>
+            <div class="row pt-3 pr-0 col-lg-6 background-row align-items-center border rounded">
+                <h2 class="col-12 font-carrinho font-preco">Opções de Entrega</h2>
                 <div class="row align-items-center pl-3">
                     <div class="form-check col-lg-12 pt-2">
                         <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1"
                             value="opcao1" checked>
                         <label class="form-check-label" for="exampleRadios1">
-                            Cartão de Crédito
+                            Endereço de Cadastro
                         </label>
+                    </div>
+                    <div class="row align-items-center pl-3 pb-2">
+                        <div class="form-check col-lg-12 pt-3">
+                            <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2"
+                                value="opcao2">
+                            <label class="form-check-label" for="exampleRadios2">
+                                Novo Endereço
+                            </label>
+                        </div>
                     </div>
                 </div>
                 <form class="no-gutters">
                     <div class="form-row col-lg-12 pt-2">
                         <div class="row col-12 pr-0 align-items-center justify-content-end">
-                            <h2 class="col-auto font-cartao">Nome</h2>
-                            <input type="text" class="form-control col-8" placeholder="">
+                            <h2 class="col-auto font-cartao">Endereço</h2>
+                            <input type="text" class="form-control col-8" placeholder="Ex: Av. Paulista, 1374">
+                        </div>
+                        <div class="row col-12 pr-0 pt-3 align-items-center justify-content-end">
+                            <h2 class="col-auto font-cartao">Bairro</h2>
+                            <input type="text" class="form-control col-8" placeholder="Ex: Bela Vista">
                         </div>
                         <div class="row col-12 pr-0 align-items-center justify-content-end pt-3">
-                            <h2 class="col-3 col-lg-auto font-cartao">Numero Cartão</h2>
-                            <input type="text" class="form-control col-8" placeholder="">
+                            <h2 class="col-3 col-lg-auto font-cartao">Cidade</h2>
+                            <input type="text" class="form-control col-8" placeholder="Ex: São Paulo">
+                        </div>
+                        <div class="row col-12 pr-0 align-items-center justify-content-end pt-3">
+                            <h2 class="col-3 col-lg-auto font-cartao">Estado</h2>
+                            <input type="text" class="form-control col-8" placeholder="Ex: São Paulo">
                         </div>
                         <div class="row col-12 pr-0 align-items-center justify-content-end">
-                            <h2 class="col-auto pt-3 mb-0 col-lg-auto font-cartao">Validade</h2>
+                            <h2 class="col-auto pt-3 mb-0 col-lg-auto font-cartao">CEP</h2>
                             <div class="row col-8 no-gutters px-0 align-items-center">
-                                <div class="col-3 pt-3">
-                                    <select class="custom-select my-1 mr-sm-2 pr-0 pl-1 text-center"
-                                        id="inlineFormCustomSelectPref">
-                                        <option selected>MM</option>
-                                        <option value="1">01</option>
-                                        <option value="2">02</option>
-                                        <option value="3">03</option>
-                                        <option value="1">04</option>
-                                        <option value="2">05</option>
-                                        <option value="3">06</option>
-                                        <option value="1">07</option>
-                                        <option value="2">08</option>
-                                        <option value="3">09</option>
-                                        <option value="1">10</option>
-                                        <option value="2">11</option>
-                                        <option value="3">12</option>
-                                    </select>
+                                <div class="col-8 pt-3">
+                                    <input type="text" class="form-control col-8" placeholder="Ex: 01310-916">
                                 </div>
-                                <div class="col-2 text-center pt-3">
-                                    <h2 class="font-produto">/</h2>
-                                </div>
-                                <div class="col-4 pt-3">
-                                    <select class="custom-select my-1 mr-sm-2 pr-0 pl-1 text-center"
-                                        id="inlineFormCustomSelectPref">
-                                        <option selected>AAAA</option>
-                                        <option value="1">2019</option>
-                                        <option value="2">2020</option>
-                                        <option value="3">2021</option>
-                                        <option value="1">2022</option>
-                                        <option value="2">2023</option>
-                                        <option value="3">2024</option>
-                                        <option value="1">2025</option>
-                                        <option value="2">2026</option>
-                                        <option value="3">2027</option>
-                                        <option value="1">2028</option>
-                                        <option value="2">2029</option>
-                                        <option value="3">2030</option>
-                                    </select>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="row col-12 pr-0 align-items-center justify-content-end">
-                            <h2 class="col-3 pt-3 mb-0 font-cartao">Codigo Segurança</h2>
-                            <div class="row col-8 pt-3 no-gutters px-0">
-                                <input type="text" class="form-control col-2 text-center" placeholder="">
                             </div>
                         </div>
                     </div>
                 </form>
-
-                <div class="row align-items-center pl-3 pb-2">
-                    <div class="form-check col-lg-12">
-                        <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2"
-                            value="opcao2">
-                        <label class="form-check-label" for="exampleRadios2">
-                            PayPal
-                        </label>
-                    </div>
-                </div>
             </div>
         </div>
     </section>
 </body>
-
-
-
 
 <?php require_once('footer.php'); ?>
