289abfc457bea1dcf243197c1ea8ef43e4a2d332
EduFStack <55258174+EduFStack@users.noreply.github.com>
2020-01-20 20:55:04 -0300

    trabalhando no carrinho

--- carrinho.php
+++ carrinho.php
@@ -1,12 +1,12 @@
 <?php require_once('header.php'); ?>
 
 <body class="background-carrinho">
-    <section class="container-fluid">
-        <div class="row justify-content-between pl-5">
+    <section class="container-fluid px-0">
+        <div class="row col-lg-12 col-sm-12 justify-content-between px-1 mx-0">
             <div
-                class="row pr-2 col-lg-8 col-md-12 col-sm-12 my-3 align-self-start justify-content-between align-items-start">
-                <div class="row col-12 background-row pl-0 border rounded">
-                    <img src="img/produtos/audio-7.jpg" alt="produto" class="pl-0 col-lg-1 col-md-12">
+                class="mx-0 px-0 pl-lg-3 row col-lg-8 col-md-12 col-12 my-3 align-self-start justify-content-center align-items-start">
+                <div class="row col-12 background-row px-0 border rounded">
+                    <img src="img/produtos/audio-7.jpg" alt="produto" class="px-0 col-lg-1 col-md-12 col-12">
                     <div class="row col-11 my-2 justify-content-between align-items-center">
                         <h2 class="col-auto pr-0 font-produto">Nome do produto</h2>
                         <h2 class="col-auto pr-0 font-preco">Preço</h2>
@@ -32,7 +32,7 @@
                 <div class="row pt-2 col-12 px-0 background-row align-items-center border rounded">
                     <h2 class="col-12 font-carrinho font-preco">Opções de Modelos:</h2>
                     <form class="form-inline col-12">
-                        <select class="col-12 custom-select " id="inlineFormCustomSelectPref">
+                        <select class="col-10 custom-select " id="inlineFormCustomSelectPref">
                             <option selected>Selecione a Opção</option>
                             <option value="1">Um</option>
                             <option value="2">Dois</option>
@@ -41,35 +41,35 @@
                     </form>
                 </div>
             </div>
-            <div class="row col-lg-4 col-md-12">
-                <div class="row pb-2 my-3 col-12 background-row border rounded">
+            <div class="row col-lg-4 col-md-12 pl-lg-4 px-0 mx-0 mx-lg-0">
+                <div class="row pb-2 my-3 px-0 mx-0 col-12 background-row border rounded">
                     <div class="row py-1 mt-2 col-auto">
-                        <h2 class="font-produto">Detalhes da Compra</h2>
+                        <h2 class="font-produto px-1">Detalhes da Compra</h2>
                     </div>
                     <hr class="col-10 mt-1">
                     <div class="row col-12 pr-0 justify-content-between">
-                        <h3 class="col-auto px-0 font-menu">Quantidade</h3>
-                        <h3 class="col-auto font-menu">1</h3>
+                        <h3 class="col-auto px-1 font-menu">Quantidade</h3>
+                        <h3 class="col-auto px-0 font-menu">1</h3>
                     </div>
                     <div class="row col-12 pr-0 justify-content-between">
-                        <h3 class="col-auto px-0 font-menu">Subtotal</h3>
-                        <h3 class="col-auto font-menu">R$ 99,99</h3>
+                        <h3 class="col-auto px-1 font-menu">Subtotal</h3>
+                        <h3 class="col-auto px-0 font-menu">R$ 99,99</h3>
                     </div>
                     <div class="row col-12 pr-0 justify-content-between">
-                        <h3 class="col-auto px-0 font-menu">Frete</h3>
-                        <h3 class="col-auto font-menu">R$ 23,99</h3>
+                        <h3 class="col-auto px-1 font-menu">Frete</h3>
+                        <h3 class="col-auto px-0 font-menu">R$ 23,99</h3>
                     </div>
                     <div class="row col-12 pr-0 justify-content-between">
-                        <h3 class="col-auto px-0 font-menu">Total</h3>
-                        <h3 class="col-auto font-menu">R$ 123,98 </h3>
+                        <h3 class="col-auto px-1 font-menu">Total</h3>
+                        <h3 class="col-auto px-0 font-menu">R$ 123,98 </h3>
                     </div>
                     <hr class="col-10 mt-2">
-                    
+
                 </div>
             </div>
         </div>
     </section>
-    <section class="container-fluid">
+    <section class="container-fluid px-0">
         <div class="row pl-5 col-lg-8 col-md-12 col-sm-12 pr-0 justify-content-between">
             <div class="row pt-2 pr-0 col-lg-6 background-row align-items-center border rounded">
                 <h2 class="col-12 mb-0 pt-1 font-carrinho font-preco">Opções de Pagamento</h2>
@@ -210,10 +210,10 @@
         </div>
     </section>
     <section class="container-fluid">
-    <div class="row pt-3 col-8 justify-content-end ">
-                        <button class="btn btn-primary col-auto">Finalizar Compra</button>
-                    </div>
-                    </section>
+        <div class="row pt-3 col-8 justify-content-end ">
+            <button class="btn btn-primary col-auto">Finalizar Compra</button>
+        </div>
+    </section>
 </body>
 
 <?php require_once('footer.php'); ?>
